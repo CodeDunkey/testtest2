6a541132258f61b1f1492a91be2a1e8ad53c1fa1
Tastan Kodersen <andmarknu@hotmail.com>
2024-05-27 11:45:15 +0200

    abc1

---
+++ ConsoleApp1/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+
+        public Animal(string name, int value) { 
+            Name = name;
+            Value = value;
+        }
+
+        public SayName(string name) {
+            Console.WriteLine($"my name is {name}");
+        }
+        public SayValue(int value)
+        {
+            Console.WriteLine($"my value is {value}");
+        }
+    }
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals= new List<Animal>();
+            Animal M1 = new Animal("M1", 1);
+
+
+        }
+    }
+}
