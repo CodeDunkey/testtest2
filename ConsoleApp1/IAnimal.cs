d625acd04bd77c379b155dff9ab9c88da63eefeb
Tastan Kodersen <andmarknu@hotmail.com>
2024-05-30 05:47:06 +0200

    30.05.2024 05.46

--- ConsoleApp1/IAnimal.cs
+++ ConsoleApp1/IAnimal.cs
@@ -8,9 +8,14 @@ namespace ConsoleApp1
 {
     public interface IAnimal
     {
+        // there can not be any private signature in an interface, it has to be a public or public can be deleted
+        // example public string Name { get; set; }
+
+        // signature property
         string Name { get; set; }
         int Value { get; set; }
 
+        // signature method
         void SayName();
         void SayValue();
     }
